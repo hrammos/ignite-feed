cd15ad568460814fc78a5ede56f51ac01e942100
Henrique Ramos <henriqueramos0106@gmail.com>
2022-07-17 16:05:19 -0300

    feat: add Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css'
 
 import './global.css'
 
@@ -9,6 +12,13 @@ export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque expedita saepe molestiae necessitatibus error placeat et excepturi neque aut nostrum! Voluptas perspiciatis odit tempore voluptate, sequi doloremque harum iusto nulla.
+        </main>
+      </div>
     </div>
   )
 }
