87c964274967d9cc6d1e40bbf950d3ad2f8ca2b4
Henrique Ramos <henriqueramos0106@gmail.com>
2022-07-31 21:16:22 -0300

    add comment form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -42,6 +42,18 @@ export function Post () {
           <a href="#">#novoprojeto</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
